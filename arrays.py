3d983b9ef998759d875f6f205b61fb3715ab7649
Kolawole Ikeoluwa <kolawoleikeoluwa2018@gmail.com>
2021-04-24 10:37:38 +0100

    operations on array data structures

---
+++ arrays.py
@@ -0,0 +1,31 @@
+"""Data Structures - case study: Arrays / Lists"""
+
+new_list = [1, 2, 3]
+
+"""
+
+Operations on Data structures
+
+"""
+
+#1. Access and Read Values
+result = new_list[0]
+
+#2. Searching for an arbitrary value
+if 1 in new_list: print('True')
+
+
+"""
+#3. Inserting values (shifts every element to the right) & Deleting values ( shifts every element to the left)
+"""
+numbers = []
+len(numbers)
+
+
+numbers.append(2)
+numbers.append(200)
+
+
+numbers.extend([4, 5, 6])
+
+
