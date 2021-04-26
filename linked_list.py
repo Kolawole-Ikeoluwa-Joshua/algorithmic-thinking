59eabd68ae817e143e0db4e6e5a3188ba9fe9b21
Kolawole Ikeoluwa <kolawoleikeoluwa2018@gmail.com>
2021-04-26 17:01:52 +0100

    added linked list methods

--- linked_list.py
+++ linked_list.py
@@ -52,5 +52,53 @@ class LinkedList:
         return count
 
 
-    
+    def add(self, data):
+
+        """
+        Adds new Node containing data at head of the list
+
+        Has O(1) - constant runtime
+
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+
+    def search(self, key):
+
+        """
+        Search for first node containing data that matches the key
+        Return the node or 'None' if not found
+
+        Takes O(n) - linear runtime
+        """
+        current = self.head
+
+        while current != None:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def __repr__(self):
+        """
+        Return a string representation of the list
+        Takes O(n) - linear runtime
+        """
         
+        nodes = []
+        current = self.head
+
+        while current != None:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+
+            current = current.next_node
+        return '->'.join(nodes)
+
